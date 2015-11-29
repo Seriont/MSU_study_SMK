7bc89ac2558dc67e65078fe00e82bf521c54130c
Nikolay <nikolajskachkov@gmail.com>
2015-11-29 19:12:17 +0300

    Servers processing part is finished

--- server/server.h
+++ server/server.h
@@ -23,10 +23,12 @@ public:
 class Server 
 {
 public:
-	bool startServer(void);
-	bool stopServer(void);
-	void sendToAllMessage(void);
+	bool startServer();
+	bool process();
+	bool getMessage(string &message, User *user);
+	void sendToAllMessage(string &message, User *user);
 	void sendServerMessage(void);
+	void addNewClient();
 	bool isNameValid(void); // checking for forbidden characters
 	bool isNameUsed(void); // checking for equal usernames
 	Server() {}
@@ -52,11 +54,12 @@ bool Server::startServer()
 }
 
 
-bool Server::()
+bool Server::process()
 {
 	fd_set read_fds;
 	while (true)
 	{
+		// read_fds initialization
 		FD_ZERO(&read_fds);
 		int max_number = this->listening_socket;
 		FD_SET(this->listening_socket, &read_fds);
@@ -67,15 +70,38 @@ bool Server::()
 			if (it->socket > max_number)
 				max_number = it->socket;
 		}
+		// query handling
 		int result = select(max_number + 1, &read_fds, NULL, NULL, NULL);
 		if (result < 1)
 			return false;
+		// new client adding
 		if (FD_ISSET(this->listening_socket, &read_fds))
 		{
-			int new_socket = accept(this->listening_socket, NULL, NULL);
-			User *new_user = new User();
-			new_user->socket = new_socket;
-			this->clients.insert(new_user);
+			if (!addNewClient())
+				return false;
+		}
+		// message reading and deleting closed sockets
+		vector<User *> clear_stack;
+		for (set<User *>::iterator it = this->clients.begin();
+			it != this->clients.end(); it++)
+		{
+			if (FD_ISSET(*it, &read_fds)) {
+				string message;
+				if (!getMessage(message, it)) {
+					close(it->socket);
+					clear_stack.push_back(it);
+					continue;
+				}
+				sendToAllMessage(message, it);
+			}
+		}
+		// clients' set refreshment
+		int length = (int) clear_stack.size();
+		while (length > 0)
+		{
+			User *element = pop_back(clear_stack);
+			this->clients.erase(element);
+			length--;
 		}
 	}
 }
